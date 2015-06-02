7dcb0b17973b8a0965a09795d5866a6ae1048eb5
MarthaLRigby <martha.l.rigby@gmail.com>
2015-06-03 00:39:06 +0100

    Ex 6-2

---
+++ 6-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Book
+    {
+        private string title;
+        private string author;
+        private string ISBN;
+        public void read()
+        {
+            //the read method
+        }
+        public void shelve()
+        {
+            //the shelve method
+        }
+    }
+}
